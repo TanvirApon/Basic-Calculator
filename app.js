94e23e79edadf56392757bbc2da602818c8a6acb
TanvirApon <tanvirhossainapon@gmail.com>
2024-09-15 09:54:12 +0600

    Update with variables

--- app.js
+++ app.js
@@ -1,87 +1,71 @@
 console.log("Javascipt Code has been added");
-    
 
-function checker(first,second){
+let first, second;
 
+function checker(num1,num2){
     console.log("Checker Function has been clicked");
+
+    first = parseFloat(document.getElementById("first_number").value);
+    second = parseFloat(document.getElementById("second_number").value);
 
     // check if the input is valid or not
     if(isNaN(first) || isNaN(second)){  
-        document.getElementById("first_number").value=" ";
-        document.getElementById("second_number").value=" ";
-        document.getElementById("result").value=" ";
+        document.getElementById("first_number").value = " ";
+        document.getElementById("second_number").value = " ";
+        document.getElementById("result").value = " ";
         alert("Invalid input");
     }
- 
 }
-
 
 function add(){
-    
     console.log("add function clicked");
+    checker();
+    console.log(first);
+    console.log(second);
 
-    const first=parseFloat(document.getElementById("first_number").value);
-    const second=parseFloat(document.getElementById("second_number").value);
-
-    checker(first,second);
-
-    const result=parseFloat(first+second);
-
-    document.getElementById("result").value=result;
+    const result = parseFloat(first + second);
+    
+    document.getElementById("result").value = result;
 }
-
 
 function minus(){
-     
     console.log("Minus function clicked");
-    const first=parseFloat(document.getElementById("first_number").value);
-    const second=parseFloat(document.getElementById("second_number").value);
+    checker();
+    console.log(first);
+    console.log(second);
 
-    checker(first,second);
+    const result = parseFloat(first-second);
 
-    const result=parseFloat(first-second);
-
-    document.getElementById("result").value=result;
- 
+    document.getElementById("result").value = result;
 }
 
-
 function multiplication(){
-     
     console.log("multiplication function clicked");
+    checker();
+    console.log(first);
+    console.log(second);
 
-    const first=parseFloat(document.getElementById("first_number").value);
-    const second=parseFloat(document.getElementById("second_number").value);
+    const result = parseFloat(first * second);
 
-    checker(first,second);
-
-    const result=parseFloat(first*second);
-
-    document.getElementById("result").value=result;
+    document.getElementById("result").value = result;
 }
 
 function divison(){
-     
     console.log("divison function clicked");
-
-    const first=parseFloat(document.getElementById("first_number").value);
-    const second=parseFloat(document.getElementById("second_number").value);
-
-    checker(first,second);
+    checker();
+    console.log(first);
+    console.log(second);
 
     const result=parseFloat(first/second);
 
-    document.getElementById("result").value=result;
- 
+    document.getElementById("result").value = result;
 }
- 
-
 
 function clear_all(){
     console.log("Clear has been clicked");
 
-    document.getElementById("first_number").value=" ";
-    document.getElementById("second_number").value=" ";
-    document.getElementById("result").value=" ";
+    document.getElementById("first_number").value = "";
+    document.getElementById("second_number").value = "";
+    document.getElementById("result").value = "";
 }
  
